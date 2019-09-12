e5cbcae7c35b3715f06c168287c78a3b0118e625
Yan Arlex <yavallejor@gmail.com>
2019-09-12 17:31:46 -0500

    initial commit

---
+++ yan.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Yan
+ * Plugin URI:        https://aprendiendoando.com/plugins/yan
+ * Description:       Yan Plugin Demo
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Yan Arlex Vallejo 
+ * Author URI:        https://aprendiendoando.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+
+
+
